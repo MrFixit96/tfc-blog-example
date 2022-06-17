64ac54a29ea2b6a3c5db2131f85b6c4e378e67cf
MrFixit96 <james@janderton.com>
2022-06-16 22:24:28 -0500

    added keypair and route table entry to ig

--- two-tier-tfc-demo-app/aws/main.tf
+++ two-tier-tfc-demo-app/aws/main.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = var.aws_region
 }
 
+data "aws_key_pair" "us-east-1" {
+  key_name = var.aws_key_pair
+}
+
+data "aws_route_table" "two-tier-tfe-demo-app" {
+  vpc_id = aws_vpc.two-tier-tfe-demo-app.id
+}
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -20,13 +28,28 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_vpc" "two-tier-tfe-demo-app" {
   cidr_block = "10.0.0.0/16"
+  tags                   = var.aws_instance_tags
 }
 
 resource "aws_subnet" "two-tier-tfe-demo-app" {
   vpc_id                  = aws_vpc.two-tier-tfe-demo-app.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
+  tags                   = var.aws_instance_tags
 }
+
+resource "aws_internet_gateway" "two-tier-tfe-demo-app" {
+  vpc_id = "${aws_vpc.two-tier-tfe-demo-app.id}"
+  tags                   = var.aws_instance_tags
+}
+
+resource "aws_route" "two-tier-tfe-demo-app-out" {
+  route_table_id            = data.aws_route_table.two-tier-tfe-demo-app.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id                = aws_internet_gateway.two-tier-tfe-demo-app.id
+  depends_on                = [data.aws_route_table.two-tier-tfe-demo-app]
+}
+
 
 resource "aws_security_group" "two-tier-tfe-demo-app" {
   name   = "two-tier-tfe-demo-app-sg"
@@ -36,8 +59,16 @@ resource "aws_security_group" "two-tier-tfe-demo-app" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = ["10.0.0.0/16", "140.177.227.38/32"]
   }
+
+  ingress {
+    from_port   = 0
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16", "140.177.227.38/32"]
+  }
+
 
   egress {
     from_port   = 0
@@ -45,10 +76,6 @@ resource "aws_security_group" "two-tier-tfe-demo-app" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-resource "aws_internet_gateway" "two-tier-tfe-demo-app" {
-  vpc_id = "${aws_vpc.two-tier-tfe-demo-app.id}"
 }
 
 resource "aws_elb" "two-tier-tfe-demo-app" {
@@ -73,6 +100,12 @@ resource "aws_instance" "two-tier-tfe-demo-app" {
   vpc_security_group_ids = [aws_security_group.two-tier-tfe-demo-app.id]
   subnet_id              = aws_subnet.two-tier-tfe-demo-app.id
   tags                   = var.aws_instance_tags
+  key_name		 = data.aws_key_pair.us-east-1.key_name
+  user_data = <<EOF
+#!/bin/bash
+sudo apt install -y nginx
+EOF
+
 }
 
 resource "aws_sqs_queue" "example" {
--- two-tier-tfc-demo-app/aws/variables.tf
+++ two-tier-tfc-demo-app/aws/variables.tf
@@ -2,6 +2,10 @@ variable "aws_region" {
   type    = string
 }
 
+variable "aws_key_pair" {
+  type    = string
+}
+
 variable "aws_instance_type" {
   type    = string
   default = "t2.micro"
